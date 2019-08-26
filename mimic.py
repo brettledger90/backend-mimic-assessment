31d438314c04a37f182742920350461d6d2792f3
brett ledger <brett.ledger90@gmail.com>
2019-08-26 15:46:16 -0400

    mimic_dict functionality_ print_mimic functionality

--- mimic.py
+++ mimic.py
@@ -50,6 +50,21 @@ import sys
 def mimic_dict(filename):
     """Returns mimic dict mapping each word to list of words which follow it."""
     # +++your code here+++
+    mimic_dict = {}
+    file = open(filename, 'r')
+    text = file.read()
+    file.close()
+    words = text.split()
+    prev = ''
+    for word in words:
+        if not prev in mimic_dict:
+            mimic_dict[prev] = [word]
+        else:
+            mimic_dict[prev].append(word)
+        prev = word
+        
+    print(mimic_dict)
+    return mimic_dict
     raise NotImplementedError("Get to Work!")
 
 
@@ -61,14 +76,19 @@ def print_mimic(mimic_dict, word):
         - Randomly select a new seed word from this word list
         - Repeat this process 200 times
     """
-    # +++your code here+++
+    for unused_i in range(200):
+        print (word)
+        nexts = mimic_dict.get(word)        
+        if not nexts:
+            nexts = mimic_dict['']  
+        word = random.choice(nexts)
     raise NotImplementedError("Get to Work!")
 
 
 # Provided main(), calls mimic_dict() and mimic()
 def main():
     if len(sys.argv) != 2:
-        print 'usage: python mimic.py file-to-read'
+        print ('usage: python mimic.py file-to-read')
         sys.exit(1)
 
     d = mimic_dict(sys.argv[1])
